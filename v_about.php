940dd006f4bc94badcac78cd1b516e9bbf936ea4
fuadhanif10 <90744084+fuadhanif10@users.noreply.github.com>
2021-09-30 21:43:53 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,21 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Fuad</li>
+ <li>Nama Belakang : Hanif</li>
+ </ul>
+ <h4>Alamat</h4>
+ <ul type="square">
+ <li> Jl. Komplek Deplu Caraka Bhuana, Gg. H.Sein Rt.003/Rw.05 No.156</li>
+ </ul>
+ <h4>Tempat Lahir</h4>
+ <ul type="disc">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Sepak Bola</li>
+ <li>Futsal</li>
+ </ul>
+</section>
